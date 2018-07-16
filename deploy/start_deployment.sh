42ef0e7a3bb35234f9a9c9c192e9334fc32ab3ea
jwdavis <jeff@jwdavis.me>
2018-07-16 15:24:12 -0700

    add cli args
    - add billing account arg
    - add sql instance name

--- deploy/start_deployment.sh
+++ deploy/start_deployment.sh
@@ -1,19 +1,20 @@
  #!/bin/bash
 
-# update file to use provided passwords
+# update files to use command-line provided args
 sed -i -e "s/<sql-pass>/$1/g" deploy-gce-demo.yaml
 sed -i -e "s/<sup-pass>/$2/g" deploy-gce-demo.yaml
+sed -i -e "s/<billing>/$3/g" deploy-gce-demo.yaml
+sed -i -e "s/<sql_name>/$4/g" deploy-gce-demo.yaml
+sed -i -e "s/<sql_name>/$4/g" ../admin/supervisor_config/proxy.conf
+sed -i -e "s/<sql_name>/$4/g" ../config.py
+sed -i -e "s/<sql_name>/$4/g" finish_deployment.sh
 
-# do sqladmin outside deployment manager due to timing delays
-# that sometime cause deployments to fail
-# gcloud service-management enable sqladmin.googleapis.com
-
-# do compute engine outside of deployment so you can delete
-# deployment without disabling compute engine api
+# enable apis
 gcloud services enable compute.googleapis.com
-
-# enable deployment manager api
 gcloud services enable deploymentmanager.googleapis.com
+gcloud services enable sql-component.googleapis.com
+gcloud services enable sqladmin.googleapis.com
+gcloud services enable pubsub.googleapis.com
 
 # create the deployment
 gcloud deployment-manager deployments create lms --config deploy-gce-demo.yaml
